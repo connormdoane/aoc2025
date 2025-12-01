a09cd3643c9444732aa4d6b1e04b57e1a98039ae
Connor <doaneconnorm@gmail.com>
2025-12-01 11:35:57 -0700

    day1 optimizations

--- 01/code.py
+++ 01/code.py
@@ -1,48 +1,42 @@
-with open("input.txt", 'r') as f:
+import sys
+
+with sys.stdin as f:
   lines = [line.rstrip() for line in f]
 
 def part1():
   dial = 50
   count = 0
   for line in lines:
-    if line[0] == 'L':
-      dial -= int(line[1:])
-      while dial < 0:
-        dial += 100
-    elif line[0] == 'R':
-      dial += int(line[1:])
-      while dial > 99:
-        dial %= 100
-    if dial == 0:
-      count += 1
+    sign = 1 if line[0] == 'R' else -1
+    dial += sign * int(line[1:])
+    dial %= 100
+    count += (dial == 0)
   print(count)
-
-def move_dial(direction, count, dial, start_count):
-  dial_res = dial
-  res = start_count
-  if direction == 'L':
-    for i in range(count):
-      dial_res -= 1
-      if dial_res == 0:
-        res += 1
-      if dial_res == -1:
-        dial_res = 99
-  if direction == 'R':
-    for i in range(count):
-      dial_res += 1
-      if dial_res == 100:
-        dial_res = 0
-      if dial_res == 0:
-        res += 1
-  return dial_res, res
   
 def part2():
   dial = 50
   count = 0
   for line in lines:
-    dial, count = move_dial(line[0], int(line[1:]), dial, count)
+    sign = 1 if line[0] == 'R' else -1
+    turn = dial + sign * int(line[1:])
+    count += abs(turn) // 100 + (dial > 0 and turn <= 0)
+    dial = turn % 100
   print(count)
 
+def both():
+  dial = 50
+  count1 = count2 = 0
+  for line in lines:
+    sign = 1 if line[0] == 'R' else -1
+    turn = dial + sign * int(line[1:])
+    count2 += abs(turn) // 100 + (dial > 0 and turn <= 0)
+    dial = turn % 100
+    count1 += (dial == 0)
+  print(count1)
+  print(count2)
+    
+
 if __name__ == "__main__":
-  part1()
-  part2()
+  # part1()
+  # part2()
+  both()
