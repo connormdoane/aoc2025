6246875c6055ea4760990dd2c1aeaaa771a775a1
Connor <doaneconnorm@gmail.com>
2025-12-01 18:32:44 -0700

    beautify day1 python

--- 01/code.py
+++ 01/code.py
@@ -27,8 +27,7 @@ def both():
   dial = 50
   count1 = count2 = 0
   for line in lines:
-    sign = 1 if line[0] == 'R' else -1
-    turn = dial + sign * int(line[1:])
+    turn = dial + (1 if line[0] == 'R' else -1) * int(line[1:])
     count2 += abs(turn) // 100 + (dial > 0 and turn <= 0)
     dial = turn % 100
     count1 += (dial == 0)
